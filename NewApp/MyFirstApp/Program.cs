47e24e741588e72af84d1a353c0063fa98d400ce
trang051201 <trangtrau201@gmail.com>
2024-04-04 16:46:46 +0700

    tập sử dụng Write, WriteLine, Read và ReadLine

--- NewApp/MyFirstApp/Program.cs
+++ NewApp/MyFirstApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// using Internal;
+using System;
+// // Tập sử dụng Console.Read
+// // lệnh này sử dụng để đọc và chuyển đổi cho kiểu number sang ASCII
+        Consonle.WriteLine("nhập vào số mong muốn:");
+        int a = Console.Read();
+        Consonle.WriteLine(" số được chuyển đổi thành: " + a);
+
+// // tập sử dụng Console.ReadLine
+// // lệnh ReadLine dùng để đọc dữ liệu đầu vào hoặc đã nhập vào
+// // lệnh ReadLine sử dụng cho kiểu String(chuỗi)
+        Console.WriteLine("Nhap vao du lieu nguoi dung: ");
+        string ten = Console.ReadLine();
+        string tuoi = Console.ReadLine();
+        Console.WriteLine("So tuoi cua " + ten + " la " + tuoi);
+
