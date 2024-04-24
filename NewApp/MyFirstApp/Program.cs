4e47a889f0ee3a0cd5ca02bc3930d6bac12c29a8
trang051201 <trangtrau201@gmail.com>
2024-04-24 17:01:44 +0700

    basic array

--- NewApp/MyFirstApp/Program.cs
+++ NewApp/MyFirstApp/Program.cs
@@ -1,4 +1,7 @@
-﻿using  MyFirstApp.Models;
+﻿using System.Collections.Specialized;
+using Internal;
+using System;
+using  MyFirstApp.Models;
 public class Program {
     private static void Main(string[] args) {
 
@@ -29,6 +32,49 @@ public class Program {
         std.EnterData();
         std.StudentCode = "2021050654";
         std.Display();
+
+        // khởi tạo Array
+        int intArray = new int[5];
+        // khai báo 1 phần tử 
+        int a = 10;
+        // gán giá trị của phần từ thứ 3 trong mảng
+        intArray[2] = a;
+
+        // khởi tạo mảng và gán giá trị index bằng một giá trị obj
+        Student[] stdArray  = new Student[10];
+        // khai báo phần tử kiểu int và gán giá trị bằng 10
+        Student std = new Student();
+        std.NhapThongTin(); // nhập thông tin cho đối tượng std
+        // gán giá trị của phần tử thứ 7 bằng giá trị của biến
+        stdArray[6] = std;
+
+        // nhập dữ liệu  cho mảng int (intArray) bao gồm n phần tử & sử dụng vòng lặp for
+        for (int i = 0; i < n ; i++) {
+            Console.WriteLine("nhap phan tu thu {0} : ", i); 
+            int a =  Convert.ToInt32(Console.ReadLine());
+            intArray[i] = a;
+        }
+        // nhập dữ liệu từ mảng student (stdStudent) bao gồm n phần tử
+        for (int i = 0; i<n ;i++) {
+            Console.WriteLine("nhap phan tu thu {0}", i);
+            Student std = new Student();
+            std.NhapThongTin();
+            intArray[i] = std;
+        }
+
+        // hiển thị dữ liệu  mảng int (intArray) bao gồm bao nhiêu phần tử
+        for(int i = 0; i < intArray.Count(); i++) {
+                Console.WriteLine(intArray[i] + "\t");
+        }
+
+        // hiển thị dữ liệu của mảng Student (stdStudent) bao gồm n phần tử
+        foreach (Student std in stdArray) {
+            string fName = std.FullName;
+            string address = std.Address;
+            Console.WriteLine("{0} - {1}", fName, address);
+        {
+            
+        }
     }
 
 }
