bd9c550066bc8876d9c426f92f59de3f92f29286
trang051201 <trangtrau201@gmail.com>
2024-05-09 16:55:45 +0700

    tao form submit cua person

--- MyWebMVC/Controllers/PersonController.cs
+++ MyWebMVC/Controllers/PersonController.cs
@@ -1,12 +1,19 @@
 using System;
 using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Mvc;
+using ptpmql2.Models;
 
 namespace ptpmql2.Controllers 
 {    
 public class PersonController : Controller
 {
     public IActionResult Index() { return View(); }
+    [HttpPost]
+    public IActionResult Index( Person ps){
+        string strOutput = "xin chao" + ps.PersonId + "-" + ps.FullName + "-" + ps.Address;
+        ViewBag.infoPerson = strOutput;
+        return View();
+    }
 }
 }
 
