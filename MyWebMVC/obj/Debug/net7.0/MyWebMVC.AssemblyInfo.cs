aa3c3656ebe3962909cf88cfa88c0024d060b299
trang051201 <trangtrau201@gmail.com>
2024-05-13 13:42:00 +0700

    build lại project mvc mới

--- MyWebMVC/obj/Debug/net7.0/MyWebMVC.AssemblyInfo.cs
+++ MyWebMVC/obj/Debug/net7.0/MyWebMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e975e7c4376e05faaeb5fc9e7d4eada2272138d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
